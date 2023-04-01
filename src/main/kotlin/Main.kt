8bf2e3ee4c761beb3e5c2cb9e28362406fd6b468
Kovie Niño <thekovie@gmail.com>
2023-04-01 11:05:00 +0800

    Fix formatting and bugs, Update income table

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 import java.io.File
 import java.time.Year
+import kotlin.math.round
+import kotlin.math.pow
+import kotlin.math.floor
+import java.text.DecimalFormat
 
 data class incomeRange(val min: Double, val max: Double, val additional: Double, val percent: Double)
 data class philhealthRange(val year: Int, val max_salary: Double, val rate: Double)
@@ -22,32 +26,48 @@ fun main() {
     val salary = readLine()!!.toDouble()
 
     // Compute
-    val sssFee = computeSSS(salary, readSSS)
-    val philhealthFee = computePhilhealth(salary, readPhilhealth)
-    val pagibigFee = computepagIbig(salary, readPagibig)
-    val totalContributions = sssFee + philhealthFee + pagibigFee
+    val sssFee = bankersRounding(computeSSS(salary, readSSS))
+    val philhealthFee = bankersRounding(computePhilhealth(salary, readPhilhealth))
+    val pagibigFee = bankersRounding(computepagIbig(salary, readPagibig))
+    val totalContributions = bankersRounding(sssFee + philhealthFee + pagibigFee)
 
     println("\n== COMPUTATION RESULT ==\n")
 
     // Monthly Contributions
     println("-- Monthly Contributions --")
-    println("SSS: P ${String.format("%.2f", sssFee)}")
-    println("Philhealth: P ${String.format("%.2f", philhealthFee)}")
-    println("Pag-ibig: P ${String.format("%.2f", pagibigFee)}")
-    println("Total Contributions: P ${String.format("%.2f", totalContributions)}")
+    println("SSS: P ${DecimalFormat("#,###.00").format(sssFee)}")
+    println("Philhealth: P ${DecimalFormat("#,###.00").format(philhealthFee)}")
+    println("Pag-ibig: P ${DecimalFormat("#,###.00").format(pagibigFee)}")
+    println("Total Contributions: P ${DecimalFormat("#,###.00").format(totalContributions)}")
 
     // Tax Computation
-    val incomeTax = computeIncomeTax(salary, totalContributions, readIncome)
-    val netPay = salary - incomeTax
+    val incomeTax = bankersRounding(computeIncomeTax(salary, totalContributions, readIncome))
+    val netPay = bankersRounding(salary - incomeTax)
 
     println("\n-- Monthly Contributions --")
-    println("Income Tax: P ${String.format("%.2f", incomeTax)}")
-    println("Net Pay after Tax: P ${String.format("%.2f", netPay)}")
+    println("Income Tax: P ${DecimalFormat("#,###.00").format(incomeTax)}")
+    println("Net Pay after Tax: P ${netPay}")
 
     // Deductions
     println("\n-- Deductions --")
-    println("Total Deductions: P ${String.format("%.2f", totalContributions + incomeTax)}")
-    println("Net Pay after Deductions: P ${String.format("%.2f", netPay - totalContributions)}")
+    println("Total Deductions: P ${DecimalFormat("#,###.00").format(totalContributions + incomeTax)}")
+    println("Net Pay after Deductions: P ${DecimalFormat("#,###.00").format(netPay - totalContributions)}")
+}
+
+fun bankersRounding(number: Double): Double {
+    val factor = 10.0.pow(2)
+    val rounded = round(number * factor)
+
+    return if (rounded % 2 == 0.0) {
+        rounded / factor
+    } else {
+        val floored = floor(number * factor)
+        if (floored % 2 == 0.0) {
+            floored / factor
+        } else {
+            (floored + 1) / factor
+        }
+    }
 }
 
 fun readIncomeTable(file: File): List<incomeRange> {
@@ -158,23 +178,25 @@ fun computepagIbig(n: Double, ranges: List<pagibigRange>): Double {
 }
 
 fun computeIncomeTax (salary: Double, contributions: Double, ranges: List<incomeRange>): Double {
-    val min = ranges.firstOrNull { range ->
-                (range.min == -1.0 && salary <= range.max) || // If min is -1, then n should be less than or equal to max
-                (range.max == -1.0 && salary >= range.min) || // If max is -1, then n should be greater than or equal to min
-                (salary in range.min..range.max) // Otherwise, n should be within the min and max range
-                }?.min ?: -1.0 // If no matching range is found, return -1 as an error indicator
-    val rate = ranges.firstOrNull { range ->
-                (range.min == -1.0 && salary <= range.max) || // If min is -1, then n should be less than or equal to max
-                (range.max == -1.0 && salary >= range.min) || // If max is -1, then n should be greater than or equal to min
-                (salary in range.min..range.max) // Otherwise, n should be within the min and max range
-                 }?.percent ?: -1.0 // If no matching range is found, return -1 as an error indicator
-    val additional = ranges.firstOrNull { range ->
-                (range.min == -1.0 && salary <= range.max) || // If min is -1, then n should be less than or equal to max
-                (range.max == -1.0 && salary >= range.min) || // If max is -1, then n should be greater than or equal to min
-                (salary in range.min..range.max) // Otherwise, n should be within the min and max range
-                }?.additional ?: -1.0 // If no matching range is found, return -1 as an error indicator
 
     val taxable_income = salary - contributions
+
+    val min = ranges.firstOrNull { range ->
+                (range.min == -1.0 && taxable_income <= range.max) || // If min is -1, then n should be less than or equal to max
+                (range.max == -1.0 && taxable_income >= range.min) || // If max is -1, then n should be greater than or equal to min
+                (taxable_income in range.min..range.max) // Otherwise, n should be within the min and max range
+                }?.min ?: -1.0 // If no matching range is found, return -1 as an error indicator
+    val rate = ranges.firstOrNull { range ->
+                (range.min == -1.0 && taxable_income <= range.max) || // If min is -1, then n should be less than or equal to max
+                (range.max == -1.0 && taxable_income >= range.min) || // If max is -1, then n should be greater than or equal to min
+                (taxable_income in range.min..range.max) // Otherwise, n should be within the min and max range
+                 }?.percent ?: -1.0 // If no matching range is found, return -1 as an error indicator
+    val additional = ranges.firstOrNull { range ->
+                (range.min == -1.0 && taxable_income <= range.max) || // If min is -1, then n should be less than or equal to max
+                (range.max == -1.0 && taxable_income >= range.min) || // If max is -1, then n should be greater than or equal to min
+                (taxable_income in range.min..range.max) // Otherwise, n should be within the min and max range
+                }?.additional ?: -1.0 // If no matching range is found, return -1 as an error indicator
+
     val compensation_lvl = (taxable_income - (min - 1)) * rate
 
     return additional + compensation_lvl
