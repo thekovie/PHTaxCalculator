aa396bcfabcc94a35396fcea1b4016e39477dd20
Rafa <58285574+JsRphlMrtnz@users.noreply.github.com>
2023-04-12 22:52:42 +0800

    Add introductory comment

--- R/calculator.r
+++ R/calculator.r
@@ -1,3 +1,9 @@
+#   ******************
+#   Last names: ARIZALA, ASPECTO, MARTINEZ, NIÑO
+#   Language: R
+#   Paradigm(s): Functional, Procedural
+#   ******************
+
 cat("Input salary: ")
 input <- readLines('stdin', n = 1)
 salary <- as.numeric(input)
@@ -6,7 +12,7 @@ setwd('../R/')
 # pag-ibig contribution
 pagibig_table <- read.csv('../resources/pagibig_tax.csv')
 x <- subset(pagibig_table, min <= salary & max >= salary | salary > min & max == -1)
-# print(x)
+
 pagibig <- salary * x$rate
 if (pagibig > 100) {
     pagibig <- 100
@@ -17,7 +23,7 @@ cat('Pag-Ibig:', pagibig, '\n')
 # sss contribution
 sss_table <- read.csv('../resources/sss_table.csv')
 x <- subset(sss_table, min <= salary & max >= salary | salary > min & max == -1)
-# print(x)
+
 sss <- x$ee
 contributions <- contributions + sss
 cat('SSS:', sss, '\n')
@@ -25,7 +31,7 @@ cat('SSS:', sss, '\n')
 # philhealth contribution
 philhealth_table <- read.csv('../resources/philhealth_table.csv')
 x <- subset(philhealth_table, year == 2023)
-# print(x)
+
 if (salary >= x$max_salary) {
     philhealth <- x$max_salary * 2
 } else if (salary <= x$min_salary) {
@@ -41,10 +47,12 @@ cat('PhilHealth:', philhealth, '\n')
 income_tax_table <- read.csv('../resources/income_tax_table.csv')
 taxable <- salary - contributions
 x <- subset(income_tax_table, min_range <= taxable & max_range >= taxable | taxable > min_range & max_range == -1)
+
 income_tax <- (taxable - x$min_range) * x$percentage + x$additional
+deductions <- contributions + income_tax
+
 cat('Total contributions:', contributions, '\n')
 cat('Income tax:',income_tax, '\n')
 cat('Net pay after tax:', salary - income_tax, '\n')
-deductions <- contributions + income_tax
 cat('Total deductions:', deductions, '\n')
 cat('Net pay after deductions:', salary - deductions, '\n')
